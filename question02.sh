c6a159f7a2b971116db8d6689b7a8561c8d13d0f
Foram_Mehta <91962703+forammm@users.noreply.github.com>
2024-08-04 09:51:33 +0930

    Create question02.sh

---
+++ question02.sh
@@ -0,0 +1,7 @@
+#https://github.com/forammm/assignment
+#!/bin/bash
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/forammm/assignment
+git push -u origin main
